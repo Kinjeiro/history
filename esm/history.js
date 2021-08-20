4b7b11337de7b1fda8d450a4fc113b9e5b117a36
Andrey Kuzmin <kinjeiro@gmail.com>
2021-08-20 18:58:17 +0300

    chore(build): build version 4 with fix

---
+++ esm/history.js
@@ -0,0 +1,5 @@
+export { default as createBrowserHistory } from './createBrowserHistory.js';
+export { default as createHashHistory } from './createHashHistory.js';
+export { default as createMemoryHistory } from './createMemoryHistory.js';
+export { createLocation, locationsAreEqual } from './LocationUtils.js';
+export { parsePath, createPath } from './PathUtils.js';
